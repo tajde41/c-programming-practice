4dd0c9fcbdb41212067666dbcddbb790eeef6cf2
tajde41 <ahmedshahryar66@gmail.com>
2025-11-04 00:05:12 +0600

    playing with github

--- hello.c
+++ hello.c
@@ -1,29 +1,30 @@
 // #include<stdio.h>
 // int main(){
-    
+
 //     int i;
 
-    
 //     for ( i = 0; i<=10; i++)
 //     printf("Bondhu Raka\n");
-        
+
 // return 0;
-// } 
+// }
 
+#include <stdio.h>
+int main()
+{
 
-
-
-#include<stdio.h>
-int main() {
-  
     int sallary = 500000;
 
-    if( sallary >= 50000 ) {
+    if (sallary >= 50000)
+    {
         printf("Biye hobe");
-    } else {
+    }
+    else
+    {
         printf("Biye Howa deri ase");
     }
 
-
     return 0;
 }
+
+// today is 4 November 2025 ; 12:04 AM
