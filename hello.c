35071abdce2c7b43a914ff1b33ccfc1c242321ec
tajde41 <ahmedshahryar66@gmail.com>
2025-11-04 00:01:35 +0600

    first commit in github

---
+++ hello.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+// int main(){
+    
+//     int i;
+
+    
+//     for ( i = 0; i<=10; i++)
+//     printf("Bondhu Raka\n");
+        
+// return 0;
+// } 
+
+
+
+
+#include<stdio.h>
+int main() {
+  
+    int sallary = 500000;
+
+    if( sallary >= 50000 ) {
+        printf("Biye hobe");
+    } else {
+        printf("Biye Howa deri ase");
+    }
+
+
+    return 0;
+}
